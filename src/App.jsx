bff409a5fbcc9d3ccb3383958267454e99886f10
muluken <mulukenjenber@gmail.com>
2025-03-06 23:47:45 +0300

    develop here section

--- src/App.jsx
+++ src/App.jsx
@@ -30,11 +30,13 @@
 
 import React from 'react'
 import Navbar from './components/Navbar/Navbar'
+import Hero from './components/Hero/Hero'
 
 const App = () => {
   return (
     <div>
       <Navbar/>
+      <Hero/>
     </div>
   )
 }
--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,12 +1,38 @@
-import React from 'react'
-import './Hero.css'
+import React from "react";
+import "./Hero.css";
+import profile from "../../assets/profile.jpg";
 
 const Hero = () => {
   return (
-    <div className='hero'>
-        
-    </div>
-  )
-}
+    <div className="hero">
+      <img
+         src={profile}
+        alt="muluken"
+        style={{
+          width: "250px",
+          height: "250px",
+          borderRadius: "50%",
+          objectFit: "cover",
+        }}
+      />
 
-export default Hero
+      <h1><span>I'm Muluken Jenber</span>, Full Stack Developer | Mobile & Web  .</h1>
+
+      <h2> <span>About</span> Me</h2>
+      <p>
+        I am a passionate Mobile and Web Developer with experience in building modern applications.
+        I previously worked at <strong>Scitech Valley</strong>, where I contributed to ERP and sales performance
+        tracking systems. Currently, I’m part of <strong>EthixDev</strong>, focusing on both web and mobile development,
+        using technologies like <strong>React, React Native, Node.js, Python, and Django</strong>. I’m always eager to learn
+        and bring creative solutions to life.
+      </p>
+
+      <div className="hero-action">
+        <div className="hero-connect">Connect With Me</div>
+        <div className="hero-resume">My Resume</div>
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
