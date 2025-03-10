ed936a036b946930c3b67067ee439e85d9ea49a0
muluken <mulukenjenber@gmail.com>
2025-03-10 18:26:39 +0300

    develop foter section both css an jsx

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,9 +1,41 @@
-import React from 'react'
-import './Footer.css'
+import React from "react";
+import "./Footer.css";
+import logo from "../../assets/logo.png";
+import { Mail } from "lucide-react";
+
 const Footer = () => {
   return (
-    <div>Footer</div>
-  )
-}
+    <div className="footer">
+      <div className="footer-top">
+        <div className="footer-top-left">
+          <img src={logo} alt="Logo" />
+          <p>
+            I am a Full-Stack Mobile & Web Developer passionate about creating
+            efficient solutions and eager <br />
+            to learn new technologies for better application delivery.
+          </p>
+        </div>
+        <div className="footer-top-right">
+          <div className="footer-email-input">
+            <Mail />
+            <input type="email" placeholder="Enter your email" />
+          </div>
+          <div className="footer-subscribe">Subscribe</div>
+        </div>
+      </div>
+      <hr />
+      <div className="footer-bottom">
+        <p className="footer-bottom-left">
+          © 2024 Muluken Jenber. All rights reserved.
+        </p>
+        <div className="footer-bottom-right">
+          <p>Terms of Service</p>
+          <p>Privacy Policy</p>
+          <p>Connect with me</p>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default Footer
+export default Footer;
