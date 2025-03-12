14edfbdc287c9fd696ca32d6b87ad63d54e5ba3f
muluken <mulukenjenber@gmail.com>
2025-03-12 15:10:05 +0300

    blocked nav bar menu icons

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -6,10 +6,14 @@ import dash from "../../assets/shape1.png";
 import { FaBars, FaTimes } from "react-icons/fa";
 
 const Navbar = () => {
-  const [menuOpen, setMenuOpen] = useState("home");
+  const [menuOpen, setMenuOpen] = useState(false);
 
   const toggleMenu = () => {
     setMenuOpen(!menuOpen);
+  };
+
+  const closeMenu = () => {
+    setMenuOpen(false);
   };
 
   return (
@@ -20,47 +24,51 @@ const Navbar = () => {
       </div>
       <ul className={`nav-menu ${menuOpen ? "active" : ""}`}>
         <li>
-          <Link to="home" smooth={true} duration={500} className="anchor-Link">
-            <p onClick={() => setMenuOpen("home")}>Home</p>
+          <Link to="home" smooth={true} duration={500} className="anchor-Link" onClick={closeMenu}>
+            <p>Home</p>
           </Link>
           {menuOpen === "home" ? (
             <img src={dash} style={{ width: "50px", height: "15px" }} />
           ) : null}
         </li>
         <li>
-          <Link to="about" smooth={true} duration={500} className="anchor-Link">
-            <p onClick={() => setMenuOpen("about")}>About Me</p>
+          <Link to="about" smooth={true} duration={500} className="anchor-Link" onClick={closeMenu}>
+            <p>About Me</p>
           </Link>
           {menuOpen === "about" ? (
             <img src={dash} style={{ width: "50px", height: "15px" }} />
           ) : null}
         </li>
         <li>
-          <Link to="services" smooth={true} duration={500} className="anchor-Link">
-            <p onClick={() => setMenuOpen("services")}>Services</p>
+          <Link to="services" smooth={true} duration={500} className="anchor-Link" onClick={closeMenu}>
+            <p>Services</p>
           </Link>
           {menuOpen === "services" ? (
             <img src={dash} style={{ width: "50px", height: "15px" }} />
           ) : null}
         </li>
         <li>
-          <Link to="work" smooth={true} duration={500} className="anchor-Link">
-            <p onClick={() => setMenuOpen("work")}>Portfolio</p>
+          <Link to="work" smooth={true} duration={500} className="anchor-Link" onClick={closeMenu}>
+            <p>Portfolio</p>
           </Link>
           {menuOpen === "work" ? (
             <img src={dash} style={{ width: "50px", height: "15px" }} />
           ) : null}
         </li>
         <li>
-          <Link to="contact" smooth={true} duration={500} className="anchor-Link">
-            <p onClick={() => setMenuOpen("contact")}>Contact</p>
+          <Link to="contact" smooth={true} duration={500} className="anchor-Link" onClick={closeMenu}>
+            <p>Contact</p>
           </Link>
           {menuOpen === "contact" ? (
             <img src={dash} style={{ width: "50px", height: "15px" }} />
           ) : null}
         </li>
       </ul>
-      <div className="nav-connect"> <Link className="anchor-link"  to="contact" smooth={true} duration={500}>Connect With Me</Link></div>
+      <div className="nav-connect"> 
+        <Link className="anchor-link" to="contact" smooth={true} duration={500} onClick={closeMenu}>
+          Connect With Me
+        </Link>
+      </div>
     </div>
   );
 };
