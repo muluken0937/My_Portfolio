276d3c2c19918cf989127300425c7d3b88c80cc3
muluken <mulukenjenber@gmail.com>
2025-03-23 09:54:49 +0300

    back the service to the first one

--- src/components/Services/Services.jsx
+++ src/components/Services/Services.jsx
@@ -1,51 +1,20 @@
-import React from "react";
-import { Swiper, SwiperSlide } from "swiper/react";
-import { Pagination, Navigation, Autoplay, EffectCoverflow } from "swiper/modules";
-import { ArrowRight } from "lucide-react";
-import "swiper/css";
-import "swiper/css/pagination";
-import "swiper/css/navigation";
-import "swiper/css/effect-coverflow";
-import "./Services.css";
-import theme from "../../assets/shape1.png";
-import services_data from "../../assets/services_data";
+import React from 'react';
+import { ArrowRight } from 'lucide-react';
+import './Services.css';
+import theme from '../../assets/shape1.png';
+import services_data from '../../assets/services_data';
 
 const Services = () => {
   return (
-    <div id="services" className="services">
-      {/* Title Section */}
+    <div id='services' className="services">
       <div className="services-title">
         <h1>My Services</h1>
-        <img src={theme} alt="Decorative Theme" />
+        <img src={theme} alt="" />
       </div>
-
-      {/* Swiper Slider */}
-      <Swiper
-        modules={[Pagination, Navigation, Autoplay, EffectCoverflow]}
-        effect="coverflow"
-        spaceBetween={30}
-        slidesPerView={3}
-        loop={true}
-        centeredSlides={true}
-        autoplay={{ delay: 3000, disableOnInteraction: false }}
-        pagination={{ clickable: true }}
-        navigation={true}
-        coverflowEffect={{
-          rotate: 30, // Rotation effect
-          stretch: 0,
-          depth: 100,
-          modifier: 1,
-          slideShadows: false,
-        }}
-        breakpoints={{
-          768: { slidesPerView: 2 },
-          1024: { slidesPerView: 3 },
-        }}
-        className="services-swiper"
-      >
-        {services_data.map((service, index) => (
-          <SwiperSlide key={index}>
-            <div className="services-format">
+      <div className="services-container">
+        {services_data.map((service, index) => {
+          return (
+            <div key={index} className="services-format">
               <h3>{service.s_no}</h3>
               <h2>{service.s_name}</h2>
               <p>{service.s_desc}</p>
@@ -54,9 +23,9 @@ const Services = () => {
                 <ArrowRight size={30} />
               </div>
             </div>
-          </SwiperSlide>
-        ))}
-      </Swiper>
+          );
+        })}
+      </div>
     </div>
   );
 };
